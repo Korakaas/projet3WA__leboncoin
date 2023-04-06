4d5839fa3748f888e2bc1d9389dd736a275ac536
Korakaas <goldfisch313@gmail.com>
2023-04-06 14:51:55 +0200

    création des entités

---
+++ src/Entity/Commentary.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CommentaryRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CommentaryRepository::class)]
+class Commentary
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(inversedBy: 'commentaries')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Annonce $annonce = null;
+
+    #[ORM\ManyToOne(inversedBy: 'commentaries')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $user = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAnnonce(): ?Annonce
+    {
+        return $this->annonce;
+    }
+
+    public function setAnnonce(?Annonce $annonce): self
+    {
+        $this->annonce = $annonce;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+}
