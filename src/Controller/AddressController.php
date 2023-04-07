9aa357b3b0ff7af22edfe3f9fa45751b47772555
Korakaas <goldfisch313@gmail.com>
2023-04-07 12:42:39 +0200

    crud address

---
+++ src/Controller/AddressController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Address;
+use App\Entity\User;
+use App\Form\AddressType;
+use App\Repository\AddressRepository;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
+
+class AddressController extends AbstractController
+{
+
+    private EntityManagerInterface $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
+    #[Route('/user/{user}/address', name:'app_address', methods: ['GET'])]
+    public function index(User $user, AddressRepository $addressRepository): Response
+    {
+        $addresses = $addressRepository->findBy(['user' => $user->getId()]);
+        return $this->render('address/index.html.twig', [
+            'addresses' => $addresses,
+            'user'=>$user
+        ]);
+    }
+
+    #[Route('/user/{user}/address/new', name: 'app_address_new', methods: ['GET', 'POST'])]
+    public function newAddress(User $user, Request $request): Response
+    {
+        $address = new Address();       
+        $addressForm = $this->createForm(AddressType::class, $address);
+        $address->setUser($user);
+        $addressForm->handleRequest($request);
+        if ($addressForm->isSubmitted() && $addressForm->isValid()) {
+            $this->em->persist($address);
+            $this->em->flush();
+            return $this->redirectToRoute('app_user');
+        }
+        return $this->render('address/new.html.twig', [
+            'addressForm' => $addressForm->createView(),
+        ]);
+    }
+
+    #[Route('/user/{user}/address/{address}/update', name: 'app_address_update', methods: ['GET', 'POST'])]
+    public function updateAddress(Address $address, Request $request): Response
+    {   
+        $addressForm = $this->createForm(AddressType::class, $address);
+        $addressForm->handleRequest($request);
+        if ($addressForm->isSubmitted() && $addressForm->isValid()) {
+            $this->em->persist($address);
+            $this->em->flush();
+            return $this->redirectToRoute('app_user');
+        }
+        return $this->render('address/edit.html.twig', [
+            'addressForm' => $addressForm->createView(),
+        ]);
+    }
+
+    #[Route('/user/{user}/address/{address}/delete', name: 'app_address_delete', methods: ['GET', 'DELETE'])]
+    public function deleteAddress(Address $address, User $user, Request $request): Response
+    {   
+        
+            $this->em->remove($address);
+            $this->em->flush();
+        
+
+        return $this->redirectToRoute("app_address", array('user' => $user->getId()));
+    }
+
+
+    
+}
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,16 +3,24 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
+
+    public function __construct(
+        private Security $security,
+    ){
+    }
+
     #[Route('/user', name: 'app_user')]
     public function index(): Response
     {
+        $user = $this->security->getUser();
         return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
+            'user' => $user,
         ]);
     }
 }
